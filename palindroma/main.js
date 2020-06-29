db238d5d2eab3c5d0ab6d0fc03dba33e1f9c31ec
emanuele5180 <macina5180>
2020-06-29 15:55:43 +0200

    palindroma

---
+++ palindroma/main.js
@@ -0,0 +1,22 @@
+//Palidroma
+
+
+//Chiedere all’utente di inserire una parola
+
+var parolaInserita = prompt("inserisci una parola");
+
+var parolaArray = parolaInserita.split("");
+
+console.log(parolaArray);
+
+var contrario = parolaArray.reverse("").join();
+console.log(contrario);
+
+
+//Creare una funzione per capire se la parola inserita è palindroma
+if (contrario[""] === parolaArray[""]){
+  console.log("vero");
+
+}else {
+  console.log("falso");
+}
